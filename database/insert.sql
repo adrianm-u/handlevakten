492cb9fe305937630c69142c7de9a8f459823bcb
adrianm-u <adrian.mutescu123@gmail.com>
2026-03-04 21:20:55 +0100

    refactoring to flask web application, worked on getting a dynamic product page using Flask/Jinja

--- database/insert.sql
+++ database/insert.sql
@@ -17,14 +17,12 @@ INSERT INTO categories (category_name) VALUES
 ('Fisk');
 
 INSERT INTO products (product_name, category_id) VALUES
-('Melk 1L', 1),
-('Yoghurt 500g', 1),
-('Grovbrød', 2),
-('Coca Cola 1.5L', 3),
-('Maarud Potetgull Salt 240g', 4),
-('Gilde Kjøttdeig 400g', 5),
-('Laks Porsjoner u/Skinn 4x125g Lerøy', 6),
-('Sliders Mini Brioche 480g', 2);
+('Meierismør 500g TINE', 1, 'static/images/tine_meierismor.jpg', 'TINE Meierismør er kjernet av fersk fløte.'),
+('Kjøttdeig 400g Gilde', 2, 'static/images/gilde_kjottdeig.jpg', 'Gilde Kjøttdeig er laget av norsk storfekjøtt.'),
+('Karbonader 2x100g Meny', 3, 'static/images/meny_karbonader.jpg', 'Meny Karbonader er laget av norsk storfekjøtt.'),
+('Freia Melkesjokolade 200g', 4, 'static/images/freia_melkesjokolade.jpg', 'Ren Melkesjokolade av beste kvalitet fra Freia Melkejsokolade. Norges mest solgte sjokolade.'),
+('Favorittsalami 150g Gilde', 5, 'static/images/favorittsalami_gilde.jpg', 'Gilde Favorittsalami er en saftig og smakfull salami.'),
+('Mentos Fruktrull 40g', 6, 'static/images/mentos_fruktrull.png', 'Mentos drops med fruktsmak.');
 
 INSERT INTO stores (store_name) VALUES
 ('Rema 1000'),
@@ -40,8 +38,8 @@ INSERT INTO prices (product_id, store_id, price) VALUES
 (1, 1, 22), (1, 2, 23), (1, 3, 24),
 (3, 1, 39), (3, 2, 37), (3, 3, 41),
 (4, 1, 32), (4, 2, 30), (4, 3, 33),
-(7, 1, 119), (7, 2, 125), (8, 1, 49),
-(8, 3, 53);
+(5, 1, 119), (5, 2, 125), (6, 1, 49),
+(6, 3, 53);
 
 INSERT INTO price_history (product_id, store_id, price, date) VALUES
 (1, 1, 24.00, '2026-02-20'),
@@ -54,11 +52,11 @@ INSERT INTO price_history (product_id, store_id, price, date) VALUES
 (4, 2, 31.00, '2026-02-22'),
 (4, 2, 30.00, '2026-02-23'),
 
-(7, 1, 129.00, '2026-02-20'),
-(7, 1, 119.00, '2026-02-23'),
+(5, 1, 129.00, '2026-02-20'),
+(5, 1, 119.00, '2026-02-23'),
 
-(8, 1, 55.00, '2026-02-21'),
-(8, 1, 49.00, '2026-02-23');
+(6, 1, 55.00, '2026-02-21'),
+(6, 1, 49.00, '2026-02-23');
 
 INSERT INTO wishlist (user_id, wishlist_name) VALUES
 (1, 'Admin test-liste'),
@@ -78,7 +76,7 @@ INSERT INTO wishlist_items (wishlist_id, product_id) VALUES
 (4, 4),
 (5, 6),
 (6, 6),
-(6, 7);
+(6, 4);
 
 INSERT INTO allergens (allergen_name) VALUES
 ('Melk'),
@@ -94,5 +92,5 @@ INSERT INTO product_allergens (product_id, allergen_id) VALUES
 (1, 1),
 (2, 1),
 (3, 2),
-(7, 5),
-(8, 2);
+(4, 5),
+(5, 2);
